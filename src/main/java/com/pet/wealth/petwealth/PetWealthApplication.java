d7b433696162ae176104fae2b93cf72a74ae076c
Gerardo Petitto <gerardo@tamu.edu>
2022-09-18 18:29:18 -0500

    removing front-end adding user controller

---
+++ src/main/java/com/pet/wealth/petwealth/PetWealthApplication.java
@@ -0,0 +1,13 @@
+package com.pet.wealth.petwealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetWealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetWealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pet/wealth/petwealth/PetWealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.wealth.petwealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetWealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
